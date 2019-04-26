be29a91b25ad375657bf1ed418d660f7086d8c60
Roy Fa-Lin Wang <falinw@umich.edu>
2019-04-26 00:51:58 -0400

    Cache and CSV

--- SI507project_tools.py
+++ SI507project_tools.py
@@ -14,14 +14,10 @@ class MarvelHero(object):
         self.name = charac_diction['name']
         self.description = charac_diction['description']
         self.thumbnail = charac_diction['thumbnail']['path']
-        self.wiki = charac_diction['urls'][1]['url']
+        self.detail = charac_diction['urls'][0]['url']
 
     def __str__(self):
-        return '{},{},{},{},{}'.format(self.id, self.name, self.description, self.thumbnail, self.wiki)
-        # if self.description:
-        #     return "ID: {} - {}: {}.\nLearn more here: {}".format(self.id, self.name, self.description, self.wiki)
-        # else:
-        #     return "ID: {} - Find more about {} in {}.".format(self.id, self.name, self.wiki)
+        return '{},{},{},{},{}'.format(self.id, self.name, self.description, self.thumbnail, self.detail)
 
 
 PUBLIC_KEY = "3117fd27b59beda1728b443a29c47958"
@@ -59,59 +55,41 @@ hero_list = get_hero_data_with_caching(name)
 try:
     for superhero in hero_list['data']['results']:
         instance = MarvelHero(superhero)
-        print(instance)
         superhero_list.append(instance)
-    print("Searching for keyword \"{}\" in Marvel database ...".format(name))
+    print("Searching for keyword \"{}\" in Marvel database.".format(name))
 except:
     print("\nSorry, no result matches the keyword. Please try again.")
-else:
-    if superhero_list != []:
-        # superhero = Superhero(id=instance.id, name=instance.name, description=instance.description, thumbnail=instance.thumbnail, wiki= instance.wiki)
-        # session.add(superhero)
-        # session.commit()
-        print(superhero_list)
-        print("Yay! Here is what I found:")
-        print("==================================================================================")
-        n=0
-        for i in superhero_list:
-            print("\n")
-            print(i)
-            another_lst[n].append(i.id)
-            another_lst[n].append(i.name)
-            another_lst[n].append(i.description)
-            another_lst[n].append(i.thumbnail)
-            another_lst[n].append(i.wiki)
-            n += 1
-    else:
-        print("\nSorry, no result matches the keyword. Please try again.")
-
-
-print("==================================================================================")
+# else:
+#     if superhero_list != []:
+#         print(superhero_list)
+#         print("Yay! Here is what I found:")
+#         print("==================================================================================")
+#         for i in superhero_list:
+#             print("\n")
+#             print(i)
+#     else:
+#         print("\nSorry, no result matches the keyword. Please try again.")
+#
+#
+# print("==================================================================================")
 
 
 # Part 2: dump all the superhero into one database
 
-
-
 # print("\n=============== CSV =================\n")
-#
-
-
-# Step 9:
 # Make a CSV file called "all_superhero.csv"
 
-# csv_file = open("all_superhero.CSV", "w")
-# csv_file.write("ID,Name,Description,Link,Photo\n")
-# for hero in instance_list:
-#     csv_file.write("{},{},{},{},{}\n".format(hero.id,hero.name,hero.description,hero.ththumbnail,hero.wiki)
-# csv_file.write("\n")
-# csv_file.close()
-# print("The CSV file is created successfully. Open the file and Listen to the top 10 songs we have sorted for you.\nLet's start from the latest one! Enjoy!")
-
+with open('all_superhero.csv', mode='w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(['ID', 'Name', 'Description', 'Photo', 'Link'])
+    for hero in superhero_list:
+    # writer.writerow(superhero_list)
+        csv_file.write("{},{},{},{},{}\n".format(hero.id,hero.name,hero.description.strip().replace(',', '').replace('\n','').replace('\r', ' '),hero.thumbnail,hero.detail.replace(',', '')))
+    csv_file.write("\n")
 
 # # ================ MOVIE =====================
-#
-import pandas as pd
+# #
+# import pandas as pd
 #
 # class Movie:
 #     def __init__(self, data):
